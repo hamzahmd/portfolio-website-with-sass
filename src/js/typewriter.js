5ebdd0a429d9aca1e7aea0ee1e0b690f539f5abd
Ecoham <59919892+Ecoham@users.noreply.github.com>
2021-06-13 20:52:09 +0500

    Updated

--- src/js/typewriter.js
+++ src/js/typewriter.js
@@ -2,7 +2,7 @@ class TypeWriter {
   constructor(txtElement, words, wait = 3000) {
     this.txtElement = txtElement;
     this.words = words;
-    this.txt = '';
+    this.txt = "";
     this.wordIndex = 0;
     this.wait = parseInt(wait, 10);
     this.type();
@@ -40,7 +40,7 @@ class TypeWriter {
       typeSpeed = this.wait;
       // Set delete to true
       this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+    } else if (this.isDeleting && this.txt === "") {
       this.isDeleting = false;
       // Move to next word
       this.wordIndex++;
@@ -53,39 +53,37 @@ class TypeWriter {
 }
 
 // Init On DOM Load
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 // Init App
 function init() {
-  const txtElement = document.querySelector('.txt-type');
-  const words = JSON.parse(txtElement.getAttribute('data-words'));
-  const wait = txtElement.getAttribute('data-wait');
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
 // sticky Menu Background
-window.addEventListener('scroll',function(){
-  if (window.scrollY>150){
-    document.querySelector('#nav').style.opacity=0.9;  
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#nav").style.opacity = 0.9;
   } else {
-    document.querySelector('#nav').style.opacity=1;  
+    document.querySelector("#nav").style.opacity = 1;
   }
 });
 //smooth scrolling
-$('#nav a, .btn').on('click', function(event){
-  if(this.hash !== ''){
+$("#nav a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $('html, body').animate(
+    $("html, body").animate(
       {
-        scrollTop: $(hash).offset().top - 50
+        scrollTop: $(hash).offset().top - 100,
       },
       800
     );
   }
-}); 
-
-
+});
 
